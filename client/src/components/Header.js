01d4f591caac74dd2c0b0fa774a71fc6b531d4de
Zach North <zrnorth@gmail.com>
2019-01-28 13:45:13 -0800

    Added auth checks in header, login and logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null: // Still waiting for request to resolve
+        return <div />;
+      case false: // logged out
+        return (
+          <li>
+            <a href="/auth/google">Log In with Google</a>
+          </li>
+        );
+      default:
+        // logged in
+        return (
+          <li>
+            <a href="/api/logout">Log out</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -8,15 +29,15 @@ class Header extends React.Component {
           <a href="#" className="left brand-logo">
             Emaily
           </a>
-          <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
