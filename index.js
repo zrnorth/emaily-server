484ad5d375fb18c0c9741a924e7a1a7fcf2f6bde
Zach North <zrnorth@gmail.com>
2019-01-28 16:31:09 -0800

    fix for heroku prod

--- index.js
+++ index.js
@@ -31,5 +31,17 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve prod assets
+  app.use(express.static("client/build"));
+
+  // Express will serve index.html if it doesn't recognize the route
+  // (delegating routing to react-router)
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
